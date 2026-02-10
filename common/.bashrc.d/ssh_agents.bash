749529e703aaecedc9dcdb0d76aa81463e2fd95a
Luke Williams <git@luckycold.aleeas.com>
2026-02-10 11:45:57 -0600

    Make proton more reslient and priortized

--- common/.bashrc.d/ssh_agents.bash
+++ common/.bashrc.d/ssh_agents.bash
@@ -3,10 +3,7 @@ PROTON_PASS_AGENT_SOCK="$HOME/.ssh/proton-pass-agent.sock"
 if [ -S "$PROTON_PASS_AGENT_SOCK" ]; then
   export SSH_AUTH_SOCK="$PROTON_PASS_AGENT_SOCK"
   SSH_AGENT_ACTIVE=1
-fi
-
-# Bitwarden SSH Agent setup
-if command -v bitwarden-desktop &> /dev/null && [ -z "$SSH_AGENT_ACTIVE" ]; then
+elif command -v bitwarden-desktop >/dev/null 2>&1; then
   export SSH_AUTH_SOCK="$HOME/.bitwarden-ssh-agent.sock"
   SSH_AGENT_ACTIVE=1
 fi
