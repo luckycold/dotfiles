18966ee5d9250f0af9408638359d9d6aaa4f87f7
Luke Williams <git@luckycold.aleeas.com>
2026-02-11 20:23:42 -0600

    Always load opencode in neovim

--- common/.config/nvim/lua/lucky/plugins/opencode.lua
+++ common/.config/nvim/lua/lucky/plugins/opencode.lua
@@ -15,7 +15,6 @@ return {
         anti_conceal = { enabled = false },
         file_types = { 'markdown', 'opencode_output' },
       },
-      ft = { 'markdown', 'Avante', 'copilot-chat', 'opencode_output' },
     },
     -- Optional, for file mentions and commands completion, pick only one
     'saghen/blink.cmp',
