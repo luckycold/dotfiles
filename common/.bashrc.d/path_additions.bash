f5f89b3cf307c237b5225fd9e4843ef8203aba82
Luke Williams <git@luckycold.aleeas.com>
2026-03-03 12:03:32 -0600

    Add brew path addition

--- common/.bashrc.d/path_additions.bash
+++ common/.bashrc.d/path_additions.bash
@@ -1,3 +1,11 @@
+if [ -x "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+elif [ -x "/opt/homebrew/bin/brew" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+elif command -v brew >/dev/null 2>&1; then
+    eval "$(brew shellenv)"
+fi
+
 if [ -d "$HOME/.local/opt/go/bin" ]; then
     PATH="$PATH:$HOME/.local/opt/go/bin"
 fi
