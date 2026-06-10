0a9050cfce17c20a9082a7ab3658a18b7fed515e
Luke Williams <git@luckycold.aleeas.com>
2026-06-10 14:31:33 -0500

    Change monitor default resolution to 2x instead of 3x and adjust SSH agent logic

--- common/.bashrc.d/ssh_agents.bash
+++ common/.bashrc.d/ssh_agents.bash
@@ -3,7 +3,7 @@ PROTON_PASS_AGENT_SOCK="$HOME/.ssh/proton-pass-agent.sock"
 if [ -S "$PROTON_PASS_AGENT_SOCK" ]; then
   export SSH_AUTH_SOCK="$PROTON_PASS_AGENT_SOCK"
   SSH_AGENT_ACTIVE=1
-elif command -v bitwarden-desktop >/dev/null 2>&1; then
+elif [ -S "$HOME/.bitwarden-ssh-agent.sock" ]; then
   export SSH_AUTH_SOCK="$HOME/.bitwarden-ssh-agent.sock"
   SSH_AGENT_ACTIVE=1
 fi
