9327d571900e081fb1d5a6e29bb049e3c5f2623d
Luke Williams <git@luckycold.aleeas.com>
2026-09-01 14:39:44 -0500

    Remove unused autostart apps
    (cherry picked from commit 8bd754203a6336063b216c9b36e35e50fdf3c718)

--- work/.config/hypr/autostart.lua
+++ work/.config/hypr/autostart.lua
@@ -1,7 +1,7 @@
 -- Extra autostart processes.
 local home = os.getenv("HOME") or ""
 
-o.launch_on_start("/opt/teams-for-linux/teams-for-linux")
-o.launch_on_start("brave")
+o.launch_on_start("omarchy-launch-webapp https://teams.cloud.microsoft")
+o.launch_on_start("brave-origin")
 o.launch_on_start(home .. "/AppImages/oneleet.appimage --password-store=gnome-libsecret")
 o.launch_on_start("betterbird")
